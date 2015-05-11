ef62f6ee3b0b70f31d911255e1ff6768679ae253
Jiaoping xiong <xiong.143@osu.edu>
2015-05-10 23:16:48 -0400

    programming assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -4,6 +4,16 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
+	m<-NULL
+	set<-function(y) {
+		   x<<-y
+		   m<<-NULL
+		
+	}
+	get <- function() x 
+	set_inverse <- function (inverse) m <<- inverse 
+	get_inverse<- function() m
+	list(set=set, get=get, set_inverse = set_inverse, get_inverse=get_inverse)
 
 }
 
@@ -11,5 +21,15 @@ makeCacheMatrix <- function(x = matrix()) {
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
+	m<- x$get_inverse()
+	if(!is.null(m)){
+		     message ("getting cached data")
+		     return(m)
+	}
+	
         ## Return a matrix that is the inverse of 'x'
+        data<- x$get()
+        m<-solve(data, ...)
+        x$set_inverse(m)
+        m
 }
